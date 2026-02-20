1d13fca220b251807ddefe144fb92a3cbad34403
souravvoid <125134322+souravvoid@users.noreply.github.com>
2026-02-20 11:44:31 +0530

    done toplogical graph in cpp

--- week6/toplogicaldfs.cpp
+++ week6/toplogicaldfs.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-void dfs(vector<int> adj[],int n,int s ,vector<int>&vis)
+void dfs(vector<int> adj[],int n,int s ,vector<int>&vis,stack<int> &st)
 {
     vis[s] = 1;
     cout<<s<<" ";
@@ -11,28 +11,36 @@ void dfs(vector<int> adj[],int n,int s ,vector<int>&vis)
     {
         if(!vis[it])
         {
-            dfs(adj,n,it,vis);
+            dfs(adj,n,it,vis,st);
         }
     }
-
+    st.push(s);
 }
 
 void checkdfs(vector<int> adj[],int n)
 {
+    
     vector<int> vis(n,0);
+    stack<int> st;
     for(int i =0 ;i<n;i++)
     {
        if(!vis[i])
        {
-          dfs(adj,n,i,vis);
+          dfs(adj,n,i,vis,st);
        }
+    }
+    vector<int> topo;
+    while(!st.empty())
+    {
+      topo.push_back(st.top());
+      st.pop();
     }
 }
 
 int main()
 {
     int n,m;
-    cout<<"Enter value";
+    cout<<"Enter value n,m";
     cin>>n;
     cin>>m;
 
@@ -48,6 +56,7 @@ int main()
 
     }
    vector<int> vis (n,0);
-   dfs(adj,n,0,vis);
-      return 0;
+   stack<int> st;
+   dfs(adj,n,0,vis,st);
+    return 0;
 }
